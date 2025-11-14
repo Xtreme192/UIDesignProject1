d173766de240e3f448ba2d5a09ee3d9c33ae6e6c
Xtreme192 <cobralee007@gmail.com>
2025-11-14 17:38:15 -0500

    Initial

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main extends JFrame{
+
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JLabel FirstName;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+
+    public Main() {
+        setTitle("Application Form");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600,800);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
